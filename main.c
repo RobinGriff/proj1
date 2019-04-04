94901b8415007e71facf52cbcb54d68472528691
Robin Griffiths <robin.griffiths@y7mail.com>
2019-04-04 05:56:02 +0000

    started encryptRotationCipher

--- main.c
+++ main.c
@@ -1,6 +1,52 @@
 #include <stdio.h>
 
-int main() {
+void encryptRotationCipher(char message[], int key);
+void decryptRotationCipher(char cipher[], int key);
 
-  return 0;
+void encryptSubCipher(char message[], char substition[]);
+void decryptSubCipher(char cipher[], char substition[]);
+
+void decryptRotationCipherAdv(char cipher[]);
+void decryptSubCipherAdv(char cipher[]);
+
+int main()
+{
+    
+    char message[] = "yeet";
+    int key = 0;
+    
+    encryptRotationCipher(message, key);
+    
+    return 0;
+}
+
+void encryptRotationCipher(char message[], int key) 
+{
+    int i;
+    char letter;
+    
+    for (i = 0 ; message[i] != '\0' ; i++)
+    {
+        letter = message[i];
+        
+        if (letter >= 'a' || letter <= 'z')
+        {
+            letter -= 32;
+        }   else if (letter < 'A' || letter > 'Z') 
+        {
+            printf("Please enter only alphabetical characters");
+            break;
+        }
+        
+        letter = (letter + key);
+        
+        if (letter > 'Z')
+        {
+            letter = letter - 'Z' + 'A' - 1;
+        }
+        
+        message [i] = letter;
+    }
+    
+    printf ("%s", message);
 }
